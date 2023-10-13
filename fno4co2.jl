a9e4f4d2e81864fc3a6569089b07cc1dcc096808
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-13 02:01:26 -0400

    ready to run

--- fno4co2.jl
+++ fno4co2.jl
@@ -290,21 +290,21 @@ for operator in Iterators.flatten((sconvs, convs, biases, sconv_biases, projects
     init!(operator, θ)
 end
 
-# # Test Code block to Load existing weights from serially trained FNO
-# θ_save = load("./data/3D_FNO/batch_size=2_dt=0.02_ep=85_epochs=250_learning_rate=0.0001_modes=4_nt=51_ntrain=1000_nvalid=100_s=1_width=20.jld2")["θ_save"]
-# for (k, v) in θ_save
-#     haskey(θ, k) && (θ[k] = v)
-#     if !haskey(θ, k)
-#         id = dist_key(k, config.partition, comm_cart) # TODO: do not send comm_cart, send parent comm instead
+# Test Code block to Load existing weights from serially trained FNO
+θ_save = load("./data/3D_FNO/batch_size=2_dt=0.02_ep=85_epochs=250_learning_rate=0.0001_modes=4_nt=51_ntrain=1000_nvalid=100_s=1_width=20.jld2")["θ_save"]
+for (k, v) in θ_save
+    haskey(θ, k) && (θ[k] = v)
+    if !haskey(θ, k)
+        id = dist_key(k, config.partition, comm_cart) # TODO: do not send comm_cart, send parent comm instead
 
-#         for (k1, v1) in θ
-#             # Update if distributed key is in the weight dict
-#             if k1.id == id
-#                 θ[k1] = dist_value(v, config.partition, comm)
-#             end
-#         end
-#     end
-# end
+        for (k1, v1) in θ
+            # Update if distributed key is in the weight dict
+            if k1.id == id
+                θ[k1] = dist_value(v, config.partition, comm)
+            end
+        end
+    end
+end
 
 # MPI.Finalize()
 # exit()
@@ -323,16 +323,16 @@ gpu_flag && (global θ = gpu(θ))
 # forward(θ, x)
 # exit()
 
-# Test Code block to check gradient with random input
-rng = Random.seed!(rank)
+# # Test Code block to check gradient with random input
+# rng = Random.seed!(rank)
 
-x = rand(rng, DDT(lifts), Domain(lifts))
-y = rand(rng, RDT(projects[2]), Range(projects[2]))
+# x = rand(rng, DDT(lifts), Domain(lifts))
+# y = rand(rng, RDT(projects[2]), Range(projects[2]))
 
-grads_dfno = gradient(params -> loss(forward(params, x), y), θ)[1]
+# grads_dfno = gradient(params -> loss(forward(params, x), y), θ)[1]
 
-MPI.Finalize()
-exit()
+# MPI.Finalize()
+# exit()
 
 # Define raw data directory
 mkpath(datadir("training-data"))
