a2fe9b83b1c817b32d65d1019563eab31879914b
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-21 20:10:14 -0400

    train from save checkpoint

--- main.jl
+++ main.jl
@@ -17,7 +17,7 @@ MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
 
-partition = [1,1,1,1]
+partition = [1,2,2,1]
 
 @assert MPI.Comm_size(comm) == prod(partition)
 
@@ -26,14 +26,9 @@ model = DFNO_2D.Model(modelConfig)
 
 θ = DFNO_2D.initModel(model)
 
-# # To save starting weights
-# weights = "serial_start_weights"
-# labels = @strdict weights
-# saveWeights(θ, model, additional=labels, comm=comm)
-
-# # To train from a checkpoint
-# filename = "mt=4_mx=4_my=4_nblocks=1_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt_in=51_nt_out=51_nx=64_ny=64_weights=serial_start_weights"
-# DFNO_2D.loadWeights!(θ, filename, "θ_save", partition)
+# To train from a checkpoint
+filename = "exp=serial_start_mt=4_mx=4_my=4_nblocks=1_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt_in=51_nt_out=51_nx=64_ny=64.jld2"
+DFNO_2D.loadWeights!(θ, filename, "θ_save", partition)
 
 x_train, y_train, x_valid, y_valid = DFNO_2D.loadData(partition)
 
--- tests/test_diff_serial.jl
+++ tests/test_diff_serial.jl
@@ -8,23 +8,20 @@ using ParametricOperators
 # serial_1 is obtained by trianing backup script 7. this is to make sure that all serial comparisons hereon will be from running the MPI job on 1 node
 # serial_2 is obtained by running the dfno on one MPI worker
 
-serial_1 = load(projectdir("data", "3D_FNO", "batch_size=2_dt=0.02_epochs=1_learning_rate=0.0001_modes=4_nt=51_ntrain=1000_nvalid=100_s=1_width=20.jld2"))
+serial_1 = load(projectdir("weights", "DFNO_2D", "batch_size=2_dt=0.02_epochs=1_learning_rate=0.0001_modes=4_nt=51_ntrain=1000_nvalid=100_s=1_width=20.jld2"))
 serial_2 = load(projectdir("weights", "DFNO_2D", "mt=4_mx=4_my=4_nblocks=1_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt_in=51_nt_out=51_nx=64_ny=64.jld2"))
 
 θ_1 = serial_1["θ_save"]
 θ_2 = serial_2["θ_save"]
 
 for (k, v) in θ_1
-    println(k, norm(v - θ_2[k]))
-    # @assert norm(v - θ_2[k]) <= 1e-8
+    @assert norm(v - θ_2[k]) <= 1e-4 # 500 times of machine precision diff adds up
 end
 
 Loss_1 = serial_1["Loss"]
 Loss_2 = serial_2["Loss"]
 
-println(norm(Loss_1 - Loss_2))
-
-# @assert norm(Loss_1 - Loss_2) <= 1e-10
+@assert norm(Loss_1 - Loss_2) <= 1e-5
 
 # To test below use backup 8
 
