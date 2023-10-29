7edaa9fc6ce1e3f4eaf7db045e2e7a2e0610555d
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-29 19:20:48 -0400

    updated training sh

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-task=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --constraint=gpu
+#SBATCH -G 1
 #SBATCH --time=01:00:00
 #SBATCH --qos=shared
 #SBATCH --account=m3863_g
-#SBATCH --constraint=gpu
 
 export SALLOC_ACCOUNT=m3863_g
 export SBATCH_ACCOUNT=m3863_g
 
 export DFNO_3D_GPU=1
 
-srun -n 1 -c 2 julia-1.8 main.jl
+srun julia-1.8 main.jl
