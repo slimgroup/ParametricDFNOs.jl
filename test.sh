f7788bcd747bf33ddd455bcd16a5859559b5a3a9
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-28 23:29:44 -0400

    sh to try

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=00:20:00
+#SBATCH --qos=regular
+#SBATCH --account=m3863
+
+export SALLOC_ACCOUNT=m3863
+export SBATCH_ACCOUNT=m3863
+
+export DFNO_3d_GPU=0
+
+srun -n 1 julia-1.8 main.jl
