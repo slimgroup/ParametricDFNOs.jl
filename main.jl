ed75a3272b5c27c5dac627ff9c90d8b39437822f
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-29 15:07:36 -0400

    terminate condition

--- main.jl
+++ main.jl
@@ -43,7 +43,6 @@ function read_perlmutter_data(path::String, modelConfig::ModelConfig; n::Int=10)
         data = zeros(modelConfig.dtype, map(range -> length(range), indices[1:4]))
         h5open(file_name, "r") do file
             times = file[key]
-            println(length(times))
             for t in indices[4]
                 data[:, :, :, t - indices[4][1] + 1] = file[times[t]][indices[1:3]...]
             end
@@ -75,14 +74,13 @@ function read_perlmutter_data(path::String, modelConfig::ModelConfig; n::Int=10)
         dist_read_x_tensor=read_x_tensor, dist_read_y_tensor=read_y_tensor)
 
         if idx <= ntrain
-            println("Train", idx, size(x), size(x_train))
             x_train[:,:,:,:,:,idx] = x[:,:,:,:,:,1]
             y_train[:,:,:,:,:,idx] = y[:,:,:,:,:,1]
         else
-            println("Valid", idx, size(x), size(x_valid))
             x_valid[:,:,:,:,:,idx-ntrain] = x[:,:,:,:,:,1]
             y_valid[:,:,:,:,:,idx-ntrain] = y[:,:,:,:,:,1]
         end
+        idx == n && break
         idx += 1
     end
 
