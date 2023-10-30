870a60487a5b551993cc084036b6c8f9d124e93c
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-29 20:08:12 -0400

    perlmutter config

--- examples/perlmutter/train.jl
+++ examples/perlmutter/train.jl
@@ -15,14 +15,15 @@ MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
 
+dim = 20
 partition = [1,1,1,1,1]
 
 @assert MPI.Comm_size(comm) == prod(partition)
 
-modelConfig = DFNO_3D.ModelConfig(nx=20, ny=20, nz=20, nt=55, nblocks=4, partition=partition)
+modelConfig = DFNO_3D.ModelConfig(nx=dim, ny=dim, nz=dim, nt=55, nblocks=4, partition=partition)
 rank == 0 && DFNO_3D.print_storage_complexity(modelConfig, batch=2)
 
-dataset_path = "/global/cfs/projectdirs/m3863/mark/training-data/training-samples/v5/$(modelConfig.nx)³"
+dataset_path = "/global/cfs/projectdirs/m3863/mark/training-data/training-samples/v5/$(dim)³"
 x_train, y_train, x_valid, y_valid = read_perlmutter_data(dataset_path, modelConfig)
 
 model = DFNO_3D.Model(modelConfig)
