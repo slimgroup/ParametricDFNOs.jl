ee66c427e88ec87a25df47f76c78b95dd2c76aef
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-30 11:23:50 -0400

    update train

--- examples/perlmutter/train.sh
+++ examples/perlmutter/train.sh
@@ -9,12 +9,12 @@
 #SBATCH --time=00:20:00
 #SBATCH --account=m3863_g
 
-#OpenMP settings:
-export OMP_NUM_THREADS=1
-export OMP_PLACES=threads
-export OMP_PROC_BIND=spread
+# # OpenMP settings: (TODO: Figure this out)
+# export OMP_NUM_THREADS=1
+# export OMP_PLACES=threads
+# export OMP_PROC_BIND=spread
 
 export DFNO_3D_GPU=1
 
-#applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
+# Applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
 srun -n 64 -c 2 --cpu_bind=cores julia-1.8 ./train.jl
