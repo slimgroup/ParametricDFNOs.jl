1c0644a8f869f484078467176515e91e12b92bf2
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-30 00:25:06 -0400

    train setup

--- examples/perlmutter/train.sh
+++ examples/perlmutter/train.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=64
-#SBATCH --constraint=gpu
-#SBATCH -G 1
-#SBATCH --time=01:00:00
-#SBATCH --qos=shared
-#SBATCH --account=m3863_g
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH -G 4
+#SBATCH -q regular
+#SBATCH -J Test_Run_10_Epochs_20_cube 
+#SBATCH --mail-user=richardr2926@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -t 00:20:00
+#SBATCH -A m3863_g
 
-export SALLOC_ACCOUNT=m3863_g
-export SBATCH_ACCOUNT=m3863_g
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
 
-export DFNO_3D_GPU=1
-
-srun julia-1.8 ./train.jl
+#run the application:
+#applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
+srun -n 64 -c 2 --cpu_bind=cores -G 4 --gpu-bind=single:1  julia-1.8 /global/homes/r/richardr/code/dfno/examples/perlmutter/train.jl
