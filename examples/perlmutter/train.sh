5597304afa24baa33ac85ffedda33f214e0f5127
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-30 10:43:01 -0400

    test run

--- examples/perlmutter/train.sh
+++ examples/perlmutter/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -C gpu
-#SBATCH -G 4
-#SBATCH -q regular
-#SBATCH -J Test_Run_10_Epochs_20_cube 
+#SBATCH -G 1
+#SBATCH -q shared
+#SBATCH --job-name Test_Run_10_Epochs_20_cube 
 #SBATCH --mail-user=richardr2926@gmail.com
 #SBATCH --mail-type=ALL
 #SBATCH -t 00:20:00
@@ -16,4 +16,4 @@ export OMP_PROC_BIND=spread
 
 #run the application:
 #applications may perform better with --gpu-bind=none instead of --gpu-bind=single:1 
-srun -n 64 -c 2 --cpu_bind=cores -G 4 --gpu-bind=single:1  julia-1.8 ./train.jl
+srun -n 64 -c 2 --cpu_bind=cores -G 4 --gpu-bind=single:1 julia-1.8 ./train.jl
