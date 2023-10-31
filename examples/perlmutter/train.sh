0ecf80ccb52b241d3d155f2befc8b49983c487a9
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-10-31 01:53:46 -0400

    training script

--- examples/perlmutter/train.sh
+++ examples/perlmutter/train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --constraint=gpu
 #SBATCH --gpus=2
 #SBATCH --qos=regular
 #SBATCH --job-name Test_Run_10_Epochs_20_cube 
 #SBATCH --mail-user=richardr2926@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --time=01:30:00
+#SBATCH --time=03:00:00
 #SBATCH --account=m3863_g
 
 # # OpenMP settings: (TODO: Figure this out)
@@ -18,4 +18,4 @@ export PATH=$PATH:$HOME/.julia/bin
 export DFNO_3D_GPU=1
 
 # mpiexecjl --project=./ -n 64 julia-1.8 ./examples/perlmutter/train.jl
-srun --ntasks=64 --export=ALL,LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gtl_cuda.so.0 julia-1.8 ./examples/perlmutter/train.jl
+srun --ntasks=128 --export=ALL,LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gtl_cuda.so.0 julia-1.8 ./examples/perlmutter/train.jl
