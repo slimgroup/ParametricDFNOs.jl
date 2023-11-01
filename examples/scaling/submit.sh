76c0e5199d5ff3f3f4a86245c964c575a360556a
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-11-01 14:53:14 -0400

    strong scaling configs

--- examples/scaling/submit.sh
+++ examples/scaling/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # (nodes, gpus, ntasks, px, py, pz, dimx, dimy, dimz, dimt)
-CONFIGURATIONS=(
+WEAK_SCALING_CONFIGURATIONS=(
     "1 1 1 1 1 1 64 64 64 20"
     "1 2 2 2 1 1 128 64 64 20"
     "1 4 4 2 2 1 128 128 64 20"
@@ -14,7 +14,20 @@ CONFIGURATIONS=(
     "128 512 512 8 8 8 512 512 512 20"
 )
 
-for config_str in "${CONFIGURATIONS[@]}"
+STRONG_SCALING_CONFIGURATIONS=(
+    "1 1 1 1 1 1 1024 1024 1024 20"
+    "1 2 2 2 1 1 1024 1024 1024 20"
+    "1 4 4 2 2 1 1024 1024 1024 20"
+    "2 8 8 2 2 2 1024 1024 1024 20"
+    "4 16 16 4 2 2 1024 1024 1024 20"
+    "8 32 32 4 4 2 1024 1024 1024 20"
+    "16 64 64 4 4 4 1024 1024 1024 20"
+    "32 128 128 8 4 4 1024 1024 1024 20"
+    "64 256 256 8 8 4 1024 1024 1024 20"
+    "128 512 512 8 8 8 1024 1024 1024 20"
+)
+
+for config_str in "${STRONG_SCALING_CONFIGURATIONS[@]}"
 do
     config=($config_str)
     bash examples/scaling/scaling.sh "${config[0]}" "${config[1]}" "${config[2]}" "${config[3]}" "${config[4]}" "${config[5]}" "${config[6]}" "${config[7]}" "${config[8]}" "${config[9]}"
