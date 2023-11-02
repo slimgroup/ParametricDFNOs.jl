84c3fa04efe33231c5caa5d9e515c5fb92385a48
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-11-01 23:44:45 -0400

    weak temporal

--- examples/scaling/submit.sh
+++ examples/scaling/submit.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
 # (nodes, gpus, ntasks, px, py, pz, dimx, dimy, dimz, dimt)
-WEAK_SCALING_CONFIGURATIONS_THOMAS=(
-    "1 1 1 1 1 1 64 64 64 20"
-    "1 2 2 2 1 1 128 64 64 20"
-    "1 4 4 2 2 1 128 128 64 20"
-    "2 8 8 2 2 2 128 128 128 20"
-    "4 16 16 4 2 2 256 128 128 20"
-    "8 32 32 4 4 2 256 256 128 20"
-    "16 64 64 4 4 4 256 256 256 20"
-    "32 128 128 8 4 4 512 256 256 20"
-    "64 256 256 8 8 4 512 512 256 20"
-    "128 512 512 8 8 8 512 512 512 20"
-)
-
 WEAK_SPATIAL_SCALING_CONFIGURATIONS=(
     "1 1 1 1 1 1 64 64 64 1"
     "1 2 2 2 1 1 128 64 64 1"
@@ -27,41 +14,44 @@ WEAK_SPATIAL_SCALING_CONFIGURATIONS=(
     "128 512 512 8 8 8 512 512 512 1"
 )
 
+WEAK_TEMPORAL_SCALING_CONFIGURATIONS=(
+    "1 1 1 1 1 1 64 64 64 20"
+    "1 1 1 1 1 1 64 64 32 20"
+    "1 1 1 1 1 1 64 32 32 20"
+    "1 1 1 1 1 1 32 32 32 20"
+    # "1 2 2 2 1 1 128 64 64 20"
+    # "1 4 4 2 2 1 128 128 64 20"
+    # "2 8 8 2 2 2 128 128 128 20"
+    # "4 16 16 4 2 2 256 128 128 20"
+    # "8 32 32 4 4 2 256 256 128 20"
+    # "16 64 64 4 4 4 256 256 256 20"
+    # "32 128 128 8 4 4 512 256 256 20"
+    # "64 256 256 8 8 4 512 512 256 20"
+    # "128 512 512 8 8 8 512 512 512 20"
+)
+
 TEST_SCALING_CONFIGURATIONS=(
-    # "1 4 4 2 2 1 64 64 32 20"
-    # "1 4 4 2 2 1 32 32 16 20"
-    # "1 4 4 2 2 1 16 16 8 20"
-    # "1 4 4 2 2 1 8 8 4 20"
+    "1 4 4 2 2 1 64 64 32 20"
+    "1 4 4 2 2 1 32 32 16 20"
+    "1 4 4 2 2 1 16 16 8 20"
+    "1 4 4 2 2 1 8 8 4 20"
     "1 4 4 2 2 1 512 512 256 1"
     "1 4 4 2 2 1 256 256 128 1"
     "1 4 4 2 2 1 128 128 64 1"
-    # "1 4 4 2 2 1 64 64 32 1"
-    # "1 4 4 2 2 1 32 32 16 1"
-    # "1 4 4 2 2 1 16 16 8 1"
-    # "1 4 4 2 2 1 8 8 4 1"
+    "1 4 4 2 2 1 64 64 32 1"
+    "1 4 4 2 2 1 32 32 16 1"
+    "1 4 4 2 2 1 16 16 8 1"
+    "1 4 4 2 2 1 8 8 4 1"
 )
 
-STRONG_SCALING_CONFIGURATIONS=(
-    "1 1 1 1 1 1 1024 1024 1024 20"
-    "1 2 2 2 1 1 1024 1024 1024 20"
-    "1 4 4 2 2 1 1024 1024 1024 20"
-    "2 8 8 2 2 2 1024 1024 1024 20"
-    "4 16 16 4 2 2 1024 1024 1024 20"
-    "8 32 32 4 4 2 1024 1024 1024 20"
-    "16 64 64 4 4 4 1024 1024 1024 20"
-    "32 128 128 8 4 4 1024 1024 1024 20"
-    "64 256 256 8 8 4 1024 1024 1024 20"
-    "128 512 512 8 8 8 1024 1024 1024 20"
-)
-
-if [[ "$1" == "weak" ]]; then
+if [[ "$1" == "weak_spatial" ]]; then
     CONFIGURATIONS=("${WEAK_SPATIAL_SCALING_CONFIGURATIONS[@]}")
-elif [[ "$1" == "strong" ]]; then
-    CONFIGURATIONS=("${STRONG_SCALING_CONFIGURATIONS[@]}")
+elif [[ "$1" == "weak_temporal" ]]; then
+    CONFIGURATIONS=("${WEAK_TEMPORAL_SCALING_CONFIGURATIONS[@]}")
 elif [[ "$1" == "test" ]]; then
     CONFIGURATIONS=("${TEST_SCALING_CONFIGURATIONS[@]}")
 else
-    echo "Invalid argument. Please specify 'weak' or 'strong'."
+    echo "Invalid argument. Please specify 'weak_spatial' or 'weak_temporal' or 'test'."
     exit 1
 fi
 
