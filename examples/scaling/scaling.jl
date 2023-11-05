ed3b36bb167cf355448beb3ffddd9a5615f3c176
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-11-05 16:15:11 -0500

    back to normal

--- examples/scaling/scaling.jl
+++ examples/scaling/scaling.jl
@@ -28,44 +28,38 @@ partition = [1,px,py,pz,1]
 
 @assert MPI.Comm_size(comm) == prod(partition)
 
-if rank == 0
-    x = rand(3000) |> gpu
+modelConfig = DFNO_3D.ModelConfig(nx=dimx, ny=dimy, nz=dimz, nt=dimt, mt=min(dimt, 4), nblocks=4, partition=partition)
+model = DFNO_3D.Model(modelConfig)
+θ = DFNO_3D.initModel(model)
+
+x_sample = rand(modelConfig.dtype, dimx * dimy * dimz * dimt * 5 ÷ prod(partition), 1)
+
+y = DFNO_3D.forward(model, θ, x_sample)
+y = DFNO_3D.forward(model, θ, x_sample)
+y_time = @elapsed DFNO_3D.forward(model, θ, x_sample)
+y_time = UTILS.dist_sum([y_time]) / size
+
+y = y .+ rand(modelConfig.dtype)
+
+function loss_helper(params)
+    global loss = sum(DFNO_3D.forward(model, params, x_sample)) # UTILS.dist_loss(DFNO_3D.forward(model, params, x_sample), y)
+    return loss
 end
-print(rank)
-CUDA.memory_status()
 
-# modelConfig = DFNO_3D.ModelConfig(nx=dimx, ny=dimy, nz=dimz, nt=dimt, mt=min(dimt, 4), nblocks=4, partition=partition)
-# model = DFNO_3D.Model(modelConfig)
-# θ = DFNO_3D.initModel(model)
+grads = gradient(params -> loss_helper(params), θ)[1]
+grads = gradient(params -> loss_helper(params), θ)[1]
+grads_time = @elapsed gradient(params -> loss_helper(params), θ)[1]
+grads_time = UTILS.dist_sum([grads_time]) / size
 
-# x_sample = rand(modelConfig.dtype, dimx * dimy * dimz * dimt * 5 ÷ prod(partition), 1)
+final_dict = @strdict nodes gpus ntasks px py pz dimx dimy dimz dimt y_time grads_time config
 
-# y = DFNO_3D.forward(model, θ, x_sample)
-# y = DFNO_3D.forward(model, θ, x_sample)
-# y_time = @elapsed DFNO_3D.forward(model, θ, x_sample)
-# y_time = UTILS.dist_sum([y_time]) / size
-
-# y = y .+ rand(modelConfig.dtype)
-
-# function loss_helper(params)
-#     global loss = sum(DFNO_3D.forward(model, params, x_sample)) # UTILS.dist_loss(DFNO_3D.forward(model, params, x_sample), y)
-#     return loss
-# end
-
-# grads = gradient(params -> loss_helper(params), θ)[1]
-# grads = gradient(params -> loss_helper(params), θ)[1]
-# grads_time = @elapsed gradient(params -> loss_helper(params), θ)[1]
-# grads_time = UTILS.dist_sum([grads_time]) / size
-
-# final_dict = @strdict nodes gpus ntasks px py pz dimx dimy dimz dimt y_time grads_time config
-
-# if rank == 0
-#     mkpath(projectdir("examples", "scaling", "results"))
-#     @tagsave(
-#         projectdir("examples", "scaling", "results", savename(final_dict, "jld2"; digits=6)),
-#         final_dict;
-#         safe=true
-#     )
-# end
+if rank == 0
+    mkpath(projectdir("examples", "scaling", "results"))
+    @tagsave(
+        projectdir("examples", "scaling", "results", savename(final_dict, "jld2"; digits=6)),
+        final_dict;
+        safe=true
+    )
+end
 
 MPI.Finalize()
