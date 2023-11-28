502b2c9b649d5a9b5463843ba1c47cd3114f3c77
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-11-28 12:33:21 -0500

    no sconv

--- src/models/DFNO_3D/forward.jl
+++ src/models/DFNO_3D/forward.jl
@@ -14,56 +14,56 @@ function forward(model::Model, θ, x::Any)
         GC.gc(true)
     end
 
-    for i in 1:model.config.nblocks
-        x = reshape((model.sconvs[i](θ) * x) + (model.convs[i](θ) * x), (model.config.nc_lift, :)) + model.sconv_biases[i](θ)
-        x = reshape(x, (model.config.nc_lift ÷ model.config.partition[1], model.config.nx ÷ model.config.partition[2], model.config.ny ÷ model.config.partition[3], model.config.nz ÷ model.config.partition[4], model.config.nt ÷ model.config.partition[5], :))
+    # for i in 1:model.config.nblocks
+    #     x = reshape((model.sconvs[i](θ) * x) + (model.convs[i](θ) * x), (model.config.nc_lift, :)) + model.sconv_biases[i](θ)
+    #     x = reshape(x, (model.config.nc_lift ÷ model.config.partition[1], model.config.nx ÷ model.config.partition[2], model.config.ny ÷ model.config.partition[3], model.config.nz ÷ model.config.partition[4], model.config.nt ÷ model.config.partition[5], :))
 
-        N = ndims(x)
-        ϵ = 1f-5
+    #     N = ndims(x)
+    #     ϵ = 1f-5
 
-        ignore() do
-            GC.gc(true)
-        end
+    #     ignore() do
+    #         GC.gc(true)
+    #     end
 
-        reduce_dims = collect(2:N)
-        scale = batch * model.config.nx * model.config.ny * model.config.nz * model.config.nt
+    #     reduce_dims = collect(2:N)
+    #     scale = batch * model.config.nx * model.config.ny * model.config.nz * model.config.nt
 
-        s = sum(x; dims=reduce_dims) |> cpu
-        reduce_mean = ParReduce(eltype(s))
-        μ = reduce_mean(s) ./ scale
+    #     s = sum(x; dims=reduce_dims) |> cpu
+    #     reduce_mean = ParReduce(eltype(s))
+    #     μ = reduce_mean(s) ./ scale
 
-        gpu_flag && (μ = μ |> gpu)
+    #     gpu_flag && (μ = μ |> gpu)
 
-        s = (x .- μ) .^ 2
+    #     s = (x .- μ) .^ 2
 
-        ignore() do
-            GC.gc(true)
-        end
+    #     ignore() do
+    #         GC.gc(true)
+    #     end
 
-        s = sum(s; dims=reduce_dims) |> cpu
-        reduce_var = ParReduce(eltype(s))
-        σ² = reduce_var(s) ./ scale
+    #     s = sum(s; dims=reduce_dims) |> cpu
+    #     reduce_var = ParReduce(eltype(s))
+    #     σ² = reduce_var(s) ./ scale
 
-        gpu_flag && (σ² = σ² |> gpu)
+    #     gpu_flag && (σ² = σ² |> gpu)
 
-        input_size = (model.config.nc_lift * model.config.nx * model.config.ny * model.config.nz * model.config.nt) ÷ prod(model.config.partition)
+    #     input_size = (model.config.nc_lift * model.config.nx * model.config.ny * model.config.nz * model.config.nt) ÷ prod(model.config.partition)
 
-        x = (x .- μ) ./ sqrt.(σ² .+ ϵ)
+    #     x = (x .- μ) ./ sqrt.(σ² .+ ϵ)
 
-        ignore() do
-            GC.gc(true)
-        end
+    #     ignore() do
+    #         GC.gc(true)
+    #     end
 
-        x = reshape(x, (input_size, :))
+    #     x = reshape(x, (input_size, :))
         
-        ignore() do
-            GC.gc(true)
-        end
+    #     ignore() do
+    #         GC.gc(true)
+    #     end
 
-        if i < model.config.nblocks
-            x = relu.(x)
-        end
-    end
+    #     if i < model.config.nblocks
+    #         x = relu.(x)
+    #     end
+    # end
 
     ignore() do
         GC.gc(true)
