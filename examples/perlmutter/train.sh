96182e37fc443da6f8227db89f6c3ba10452195d
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-01 18:21:26 -0500

    move ntasks

--- examples/perlmutter/train.sh
+++ examples/perlmutter/train.sh
@@ -2,7 +2,6 @@
 #SBATCH --nodes=16
 #SBATCH --constraint=gpu
 #SBATCH --gpus=64
-#SBATCH --ntasks=64
 #SBATCH --gpus-per-task=1
 #SBATCH --qos=regular
 #SBATCH --job-name Test_Run_200_Epochs_80_cube 
@@ -21,4 +20,4 @@ export PATH=$PATH:$HOME/.julia/bin
 export DFNO_3D_GPU=1
 export LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gtl_cuda.so.0
 
-srun --export=ALL julia-1.8 ./examples/perlmutter/train.jl
+srun --ntasks=64 --export=ALL julia-1.8 ./examples/perlmutter/train.jl
