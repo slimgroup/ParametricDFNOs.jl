42889365e9181c99bd0bed4a40750cda67b1a221
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-04 20:19:25 -0500

    new dims

--- examples/perlmutter/submit.sh
+++ examples/perlmutter/submit.sh
@@ -1,6 +1,6 @@
 # nodes gpus epochs cube_size sample_count
 
-bash examples/perlmutter/train.sh 16 64 200 20 2000
-bash examples/perlmutter/train.sh 16 64 200 40 2000
-bash examples/perlmutter/train.sh 64 256 200 80 2000
-bash examples/perlmutter/train.sh 64 256 200 160 2000
+bash examples/perlmutter/train.sh 4 16 200 20 2000
+bash examples/perlmutter/train.sh 25 100 200 40 2000
+bash examples/perlmutter/train.sh 50 200 200 80 2000
+bash examples/perlmutter/train.sh 100 400 200 160 2000
