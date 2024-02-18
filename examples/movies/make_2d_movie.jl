9899e4f0c22b4e751b49334662291500f2e7b717
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-18 10:04:10 -0500

    2d Movie

--- examples/movies/make_2d_movie.jl
+++ examples/movies/make_2d_movie.jl
@@ -4,9 +4,9 @@
 using Pkg
 Pkg.activate("./")
 
-include("../../src/models/DFNO_2D_OLD/DFNO_2D_OLD.jl")
+include("../../src/models/DFNO_2D/DFNO_2D.jl")
 
-using .DFNO_2D_OLD
+using .DFNO_2D
 using MPI
 
 using PyCall
@@ -18,23 +18,29 @@ MPI.Init()
 
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
+pe_count = MPI.Comm_size(comm)
 
-partition = [1,1,1,1]
+partition = [1,pe_count]
+samples = parse.(Int, ARGS[1])
 
 @assert MPI.Comm_size(comm) == prod(partition)
 
-modelConfig = DFNO_2D_OLD.ModelConfig(nblocks=4, partition=partition)
-dataConfig = DFNO_2D_OLD.DataConfig(modelConfig=modelConfig, ntrain=2, nvalid=0)
+modelConfig = DFNO_2D.ModelConfig(nblocks=4, partition=partition)
+dataConfig = DFNO_2D.DataConfig(modelConfig=modelConfig, ntrain=samples, nvalid=0)
 
-model = DFNO_2D_OLD.Model(modelConfig)
-θ = DFNO_2D_OLD.initModel(model)
+model = DFNO_2D.Model(modelConfig)
+θ = DFNO_2D.initModel(model)
 
 # Load Trained Weights
-filename = "mt=4_mx=4_my=4_nblocks=4_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt=51_nx=64_ny=64_p=4.jld2"
-DFNO_2D_OLD.loadWeights!(θ, filename, "θ_save", partition)
+filename = "ep=50_mt=4_mx=4_my=4_nblocks=4_nc_in=4_nc_lift=20_nc_mid=128_nc_out=1_nt=51_nx=64_ny=64_p=4.jld2"
+DFNO_2D.loadWeights!(θ, filename, "θ_save", partition)
 
-x_plot, y_plot, _, _ = DFNO_2D_OLD.loadDistData(dataConfig)
-y_predict = DFNO_2D_OLD.forward(model, θ, x_plot)
+x_plot, y_plot, _, _ = DFNO_2D.loadDistData(dataConfig)
+y_predict = DFNO_2D.forward(model, θ, x_plot)
+
+x_plot = reshape(x_plot, (modelConfig.nc_in, modelConfig.nt, modelConfig.nx, modelConfig.ny, :))
+y_plot = reshape(y_plot, (modelConfig.nc_out, modelConfig.nt, modelConfig.nx, modelConfig.ny, :))
+y_predict = reshape(y_predict, (modelConfig.nc_out, modelConfig.nt, modelConfig.nx, modelConfig.ny, :))
 
 function showanim(filename)
     base64_video = base64encode(open(filename))
@@ -46,26 +52,26 @@ fig = figure(figsize=(12, (size(x_plot,5))*4))
 function make_frame(i)
     for s in 1:size(x_plot,5)
         subplot(size(x_plot,5),4,4*(s-1)+1)
-        imshow(x_plot[1,:,:,i+1,s]')
+        imshow(x_plot[1,i+1,:,:,s]')
         title("permeability")
 
         subplot(size(x_plot,5),4,4*(s-1)+2)
-        imshow(y_plot[1,:,:,i+1,s]', vmin=0, vmax=1)
+        imshow(y_plot[1,i+1,:,:,s]', vmin=0, vmax=1)
         title("true saturation")
 
         subplot(size(x_plot,5),4,4*(s-1)+3)
-        imshow(y_predict[1,:,:,i+1,s]', vmin=0, vmax=1)
+        imshow(y_predict[1,i+1,:,:,s]', vmin=0, vmax=1)
         title("predicted saturation")
 
         subplot(size(x_plot,5),4,4*(s-1)+4)
-        imshow(5f0 .* abs.(y_plot[1,:,:,i+1,s]'-y_predict[1,:,:,i+1,s]'), vmin=0, vmax=1)
+        imshow(5f0 .* abs.(y_plot[1,i+1,:,:,s]'-y_predict[1,i+1,:,:,s]'), vmin=0, vmax=1)
         title("5X abs difference")
     end
 end
 
 withfig(fig) do
     myanim = anim.FuncAnimation(fig, make_frame, frames=size(y_predict,4), interval=80)
-    myanim[:save]("ML4Seismic_2D.mov", bitrate=-1, extra_args=["-vcodec", "libx264", "-pix_fmt", "yuv420p"])
+    myanim[:save]("2D.mp4", bitrate=-1, extra_args=["-vcodec", "libx264", "-pix_fmt", "yuv420p"])
 end
 
 # showanim("ML4Seismic.mp4") 
