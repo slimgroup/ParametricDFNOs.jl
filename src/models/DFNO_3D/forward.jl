9088f9c81ec821852774d33ab970efe41b088d45
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-18 20:26:29 -0500

    lots of gc

--- src/models/DFNO_3D/forward.jl
+++ src/models/DFNO_3D/forward.jl
@@ -2,10 +2,15 @@ function forward(model::Model, θ, x::Any)
      
     input_size = (model.config.nc_in * model.config.nx * model.config.ny * model.config.nz * model.config.nt) ÷ prod(model.config.partition)
     gpu_flag && (x = x |> gpu)
-
+    ignore() do
+        GC.gc(true)
+    end
     batch = length(x) ÷ input_size 
     x = reshape(x, (model.config.nc_in, :, batch))
     x = (model.lifts(θ) * x) + model.biases[1](θ)
+    ignore() do
+        GC.gc(true)
+    end
 
    for i in 1:model.config.nblocks
        input_size = (model.config.nc_lift * model.config.nx * model.config.ny * model.config.nz * model.config.nt) ÷ prod(model.config.partition)
@@ -15,6 +20,9 @@ function forward(model::Model, θ, x::Any)
 
        x = reshape(x, (model.config.nc_lift, :, batch))
        x2 = (model.convs[i](θ) * x) + model.sconv_biases[i](θ)
+       ignore() do
+           GC.gc(true)
+       end
 
        x = vec(x2)
        x = reshape(x, (model.config.nc_lift, model.config.nt * model.config.nx ÷ model.config.partition[1], model.config.ny * model.config.nz ÷ model.config.partition[2], :))
@@ -30,24 +38,45 @@ function forward(model::Model, θ, x::Any)
        μ = reduce_mean(s) ./ scale
 
        s = (x .- μ) .^ 2
+       ignore() do
+           GC.gc(true)
+       end
 
        s = sum(s; dims=reduce_dims)
        reduce_var = ParReduce(eltype(s))
        σ² = reduce_var(s) ./ scale
 
        x = (x .- μ) ./ sqrt.(σ² .+ ϵ)
+       ignore() do
+           GC.gc(true)
+       end
 
        if i < model.config.nblocks
            x = relu.(x)
        end
    end
    x = reshape(x, (model.config.nc_lift, :, batch))
+   ignore() do
+       GC.gc(true)
+   end
 
    x = (model.projects[1](θ) * x) + model.biases[2](θ)
+   ignore() do
+       GC.gc(true)
+   end
    x = relu.(x)
+   ignore() do
+       GC.gc(true)
+   end
 
    x = (model.projects[2](θ) * x) + model.biases[3](θ)
+   ignore() do
+       GC.gc(true)
+   end
    x = 1f0.-relu.(1f0.-relu.(x))
+   ignore() do
+       GC.gc(true)
+   end
 
    return reshape(x, (model.config.nc_out * model.config.nt * model.config.nx ÷ model.config.partition[1], model.config.ny * model.config.nz ÷ model.config.partition[2], :))
 end
