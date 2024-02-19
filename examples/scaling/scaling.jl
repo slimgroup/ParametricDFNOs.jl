8e50511150bbde9ae27e4851271f935841f3f0a0
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-19 14:46:32 -0500

    fixed modes

--- examples/scaling/scaling.jl
+++ examples/scaling/scaling.jl
@@ -28,10 +28,10 @@ config = ARGS[7]
 
 # For scaling tests, use 4 modes, training use 25% modes
 
-modesx = max(dimx÷32, 4)
-modesy = max(dimy÷32, 4)
-modesz = max(dimz÷32, 4)
-modest = max(dimt÷32, 4)
+modesx = 4 # max(dimx÷32, 4)
+modesy = 4 # max(dimy÷32, 4)
+modesz = 4 # max(dimz÷32, 4)
+modest = 4 # max(dimt÷32, 4)
 
 modelConfig = DFNO_3D.ModelConfig(nx=dimx, ny=dimy, nz=dimz, nt=dimt, mx=modesx, my=modesy, mz=modesz, mt=modest, nblocks=4, partition=partition)
 
