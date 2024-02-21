a9202cc9efcac438e67d8b5a870178f62c0110f7
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-21 16:59:35 -0500

    no if

--- memory/julia.jl
+++ memory/julia.jl
@@ -7,32 +7,19 @@ using CUDA
 # Function to convert bytes to megabytes
 bytes_to_MB(x) = x / 1024^2
 
-if true # CUDA.has_cuda_gpu()
-    # Move computations to the GPU
-    device = gpu
+W = randn(10000, 65000) |> gpu
+input_tensor = randn(65000) |> gpu
+# Define the loss function
 
-    # Create weight matrix and input tensor
-    W = randn(10000, 65000) |> device
-    input_tensor = randn(65000) |> device
-
-    # Define the loss function
-    loss(x) = sum(W * x)
-
-    for i in 1:2
-        println("\nIteration $i")
-
-        # Memory usage before the operation
-        CUDA.memory_allocated()
-
-        # Forward pass and loss
-        L = loss(input_tensor)
-
-        # Backward pass
-        @time grads = gradient(() -> loss(input_tensor), Flux.params(W))
-
-        # Memory usage after the operation
-        CUDA.memory_allocated()
-    end
-else
-    println("CUDA-enabled GPU is not available.")
+loss(x) = sum(W * x)
+for i in 1:2
+    println("\nIteration $i")
+    # Memory usage before the operation
+    CUDA.memory_allocated()
+    # Forward pass and loss
+    L = loss(input_tensor)
+    # Backward pass
+    @time grads = gradient(() -> loss(input_tensor), Flux.params(W))
+    # Memory usage after the operation
+    CUDA.memory_allocated()
 end
