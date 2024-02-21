c2a0e536d1c1debe7a8ddfcfd6cc1723245c452f
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-21 17:00:18 -0500

    status

--- memory/julia.jl
+++ memory/julia.jl
@@ -15,11 +15,11 @@ loss(x) = sum(W * x)
 for i in 1:2
     println("\nIteration $i")
     # Memory usage before the operation
-    CUDA.memory_allocated()
+    CUDA.memory_status()
     # Forward pass and loss
     L = loss(input_tensor)
     # Backward pass
     @time grads = gradient(() -> loss(input_tensor), Flux.params(W))
     # Memory usage after the operation
-    CUDA.memory_allocated()
+    CUDA.memory_status()
 end
