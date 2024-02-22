f15bc22610490bf01b1501c493a48b7c4f6ec45c
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-02-21 23:50:07 -0500

    norm

--- examples/scaling/gradient_scaling_3d.jl
+++ examples/scaling/gradient_scaling_3d.jl
@@ -15,7 +15,6 @@ using Zygote
 using DrWatson
 using ParametricOperators
 using CUDA
-using LinearAlgebra
 
 # gpu = ParametricOperators.gpu
 MPI.Init()
@@ -48,7 +47,7 @@ gpu_flag && (y_sample = y_sample |> gpu)
 @time y = DFNO_3D.forward(model, θ, x_sample)
 
 function loss_helper(params)
-    global loss = norm(vec(DFNO_3D.forward(model, params, x_sample)) - vec(y_sample)) # UTILS.dist_loss(DFNO_3D.forward(model, params, x_sample), y_sample)
+    global loss = norm()UTILS.dist_loss(DFNO_3D.forward(model, params, x_sample), y_sample)
     return loss
 end
 
