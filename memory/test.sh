bdb53ff6d9662cb478ab69e4d33125c3c8a78743
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-03-01 10:17:30 -0500

    one init

--- memory/test.sh
+++ memory/test.sh
@@ -1,3 +1,3 @@
 export JULIA_CUDA_USE_BINARY_BUILDER=false
-# julia-1.8 memory/test.jl
-julia-1.8 memory/fno_nopo_mpi_julia.jl 128 128 64 15
+julia-1.8 memory/test.jl
+# julia-1.8 memory/fno_nopo_mpi_julia.jl 128 128 64 15
