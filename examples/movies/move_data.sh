c4db9a8fe2fe920790f234041f36d838a21fee67
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-03-02 10:37:15 -0500

    v5

--- examples/movies/move_data.sh
+++ examples/movies/move_data.sh
@@ -1,4 +1,5 @@
 data=(1000419844110211017 1000660235447900098 100370370781588123 1003808605201549184 1004085618827838148)
+v_data=(2725672282015258006 2726689402663675302 27267018271693390)
 
 for folder in "${data[@]}"; do
     ssh -l richardr -i ~/.ssh/nersc perlmutter.nersc.gov "cd /pscratch/sd/r/richardr/v5/20³ && tar cz $folder" | tar zxv -C /Users/richardr2926/Desktop/Research/Code/dfno/data/DFNO_3D/v5/20³
