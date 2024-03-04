148664549f1e911e511f9ca36e9f47cfb0ae8b82
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-03-04 10:17:08 -0500

    export

---
+++ src/DFNO.jl
@@ -0,0 +1,9 @@
+module DFNO
+
+include("models/DFNO_2D/DFNO_2D.jl")
+include("models/DFNO_2D_OLD/DFNO_2D_OLD.jl")
+
+include("models/DFNO_3D/DFNO_3D.jl")
+include("models/DFNO_3D_OLD/DFNO_3D_OLD.jl")
+
+end
