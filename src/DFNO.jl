70a37cc7bb145729d97d3ab9cfe9c2ee5b4b587f
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-03-05 17:05:58 -0500

    remove old

--- src/DFNO.jl
+++ src/DFNO.jl
@@ -1,9 +1,6 @@
 module DFNO
 
 include("models/DFNO_2D/DFNO_2D.jl")
-include("models/DFNO_2D_OLD/DFNO_2D_OLD.jl")
-
 include("models/DFNO_3D/DFNO_3D.jl")
-include("models/DFNO_3D_OLD/DFNO_3D_OLD.jl")
 
 end
